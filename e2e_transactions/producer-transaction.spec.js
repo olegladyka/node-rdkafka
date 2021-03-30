becc82ab69a3e0b4bd0de58430cc919ab9c5b42c
Yordan Pavlov <y.d.pavlov@gmail.com>
2021-03-30 18:33:31 +0300

    Passing transactions test is added.

--- e2e_transactions/producer-transaction.spec.js
+++ e2e_transactions/producer-transaction.spec.js
@@ -9,37 +9,45 @@
 
 var Kafka = require('../');
 var t = require('assert');
-var crypto = require('crypto');
-
-var eventListener = require('./listener');
 
 var kafkaBrokerList = process.env.KAFKA_HOST || 'localhost:9092';
 
-var serviceStopped = false;
-
 describe('Producer', function() {
-
   var producer;
+  var consumer;
 
   describe('with dr_cb', function() {
     beforeEach(function(done) {
       producer = new Kafka.Producer({
         'client.id': 'kafka-test',
         'metadata.broker.list': kafkaBrokerList,
-        'dr_cb': true,
-        'debug': 'all'
+        'dr_msg_cb': true,
+        'debug': 'all',
+        'transactional.id': 'noderdkafka_transactions_test',
+        'enable.idempotence': true
       });
+
+      consumer = new Kafka.KafkaConsumer({
+        //'debug': 'all',
+        'metadata.broker.list': kafkaBrokerList,
+        'group.id': 'node-rdkafka-consumer-flow-example',
+        'enable.auto.commit': true,
+        'isolation.level': 'read_committed'
+      });
+
       producer.connect({}, function(err) {
         t.ifError(err);
         done();
       });
 
-      eventListener(producer);
+      //eventListener(producer);
     });
 
     afterEach(function(done) {
       producer.disconnect(function() {
-        done();
+        consumer.disconnect(function() {
+          done();
+        });
       });
     });
 
@@ -47,156 +55,49 @@ describe('Producer', function() {
       this.timeout(3000);
 
       var total = 0;
-      var max = 10000;
-      var verified_received = 0;
-      var init_transaction_timeout_ms = 200;
+      var max = 100;
+      var transactions_timeout_ms = 200;
 
       var tt = setInterval(function() {
         producer.poll();
       }, 200);
+      var topic = "test";
 
-      producer.connect();
-      producer.initTransactions(init_transaction_timeout_ms);
-      producer
-        .on('delivery-report', function(err, report) {
-          t.ifError(err);
-          t.notStrictEqual(report, undefined);
-          t.strictEqual(typeof report.topic, 'string');
-          t.strictEqual(typeof report.partition, 'number');
-          t.strictEqual(typeof report.offset, 'number');
-          verified_received++;
-          if (verified_received === max) {
-            clearInterval(tt);
-            done();
-          }
-          console.log(report);
-        });
+      consumer.on('ready', function(arg) {
+        //consumer.assign([ { offset: 0, partition: 0, topic: 'test' } ]);
+        consumer.subscribe([topic]);
 
-      // Produce
-      for (total = 0; total <= max; total++) {
-        producer.produce('test', null, Buffer.from('message ' + total), null);
-      }
+        //start consuming messages
+        consumer.consume();
 
-    });
+        producer.initTransactions(transactions_timeout_ms);
+        producer.beginTransaction();
 
-  });
+        for (total = 0; total <= max; total++) {
+          producer.produce(topic, null, Buffer.from('message ' + total), null);
+        }
 
-  describe('with_dr_msg_cb', function() {
-    beforeEach(function(done) {
-      producer = new Kafka.Producer({
-        'client.id': 'kafka-test',
-        'metadata.broker.list': kafkaBrokerList,
-        'dr_msg_cb': true,
-        'debug': 'all'
-      });
-      producer.connect({}, function(err) {
-        t.ifError(err);
-        done();
+        producer.commitTransaction(transactions_timeout_ms);
       });
 
-      eventListener(producer);
-    });
+      var counter = 0;
+      consumer.on('data', function(m) {
+        counter++;
 
-    afterEach(function(done) {
-      producer.disconnect(function() {
-        done();
-      });
-    });
-
-    it('should produce a message with a payload and key', function(done) {
-      this.timeout(3000);
-
-      var tt = setInterval(function() {
-        producer.poll();
-      }, 200);
-
-      producer.once('delivery-report', function(err, report) {
-        clearInterval(tt);
-        t.ifError(err);
-        t.notStrictEqual(report, undefined);
-        t.strictEqual(typeof report.topic, 'string');
-        t.strictEqual(typeof report.partition, 'number');
-        t.strictEqual(typeof report.offset, 'number');
-        t.ok(report.key.toString(), 'key');
-        t.equal(report.value.toString(), 'hai');
-        done();
+        consumer.commit(m);
+        if (counter == max) {
+          clearInterval(tt);
+          done()
+        }
       });
 
-      producer.produce('test', null, Buffer.from('hai'), 'key');
-    });
-
-    it('should produce a message with an empty payload and empty key (https://github.com/Blizzard/node-rdkafka/issues/117)', function(done) {
-      this.timeout(3000);
-
-      var tt = setInterval(function() {
-        producer.poll();
-      }, 200);
-
-      producer.once('delivery-report', function(err, report) {
-        clearInterval(tt);
-        t.ifError(err);
-        t.notStrictEqual(report, undefined);
-
-        t.strictEqual(typeof report.topic, 'string');
-        t.strictEqual(typeof report.partition, 'number');
-        t.strictEqual(typeof report.offset, 'number');
-        t.equal(report.key.toString(), '', 'key should be an empty string');
-        t.strictEqual(report.value.toString(), '', 'payload should be an empty string');
-        done();
+      consumer.on('event.error', function(err) {
+        console.error('Error from consumer');
+        console.error(err);
       });
 
-      producer.produce('test', null, Buffer.from(''), '');
+      consumer.connect();
     });
-
-    it('should produce a message with a null payload and null key  (https://github.com/Blizzard/node-rdkafka/issues/117)', function(done) {
-      this.timeout(3000);
-
-      producer.setPollInterval(10);
-
-      producer.once('delivery-report', function(err, report) {
-        t.ifError(err);
-        t.notStrictEqual(report, undefined);
-
-        t.strictEqual(typeof report.topic, 'string');
-        t.strictEqual(typeof report.partition, 'number');
-        t.strictEqual(typeof report.offset, 'number');
-        t.strictEqual(report.key, null, 'key should be null');
-        t.strictEqual(report.value, null, 'payload should be null');
-        done();
-      });
-
-      producer.produce('test', null, null, null);
-    });
-
-    it('should produce an int64 key (https://github.com/Blizzard/node-rdkafka/issues/208)', function(done) {
-
-      var v1 = 0x0000000000000084;
-      var arr = new Uint8Array(8);
-      arr[0] = 0x00;
-      arr[1] = 0x00;
-      arr[2] = 0x00;
-      arr[3] = 0x00;
-      arr[4] = 0x00;
-      arr[5] = 0x00;
-      arr[6] = 0x00;
-      arr[7] = 84;
-      var buf = Buffer.from(arr.buffer);
-
-      producer.setPollInterval(10);
-
-      producer.once('delivery-report', function(err, report) {
-        t.ifError(err);
-        t.notStrictEqual(report, undefined);
-
-        t.deepEqual(buf, report.key);
-        done();
-      });
-
-      producer.produce('test', null, null, Buffer.from(arr.buffer));
-
-      this.timeout(3000);
-    });
-
   });
 
 });
